7ff364196b3eb2ec5a04bdb32dee01f5eafd24d8
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-09-07 21:36:22 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/QuickSheet/GDataPlugin/RunTime/Text1Data.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+///
+/// !!! Machine generated code !!!
+/// !!! DO NOT CHANGE Tabs to Spaces !!!
+///
+[System.Serializable]
+public class Text1Data
+{
+  [SerializeField]
+  string a;
+  public string A { get {return a; } set { a = value;} }
+  
+  [SerializeField]
+  string b;
+  public string B { get {return b; } set { b = value;} }
+  
+  [SerializeField]
+  string c;
+  public string C { get {return c; } set { c = value;} }
+  
+}
