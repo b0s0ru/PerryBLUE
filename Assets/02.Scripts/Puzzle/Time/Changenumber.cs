5eadb4138610080a05b33528a2d9258ed6e7a000
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-09-09 03:22:10 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/Puzzle/Time/Changenumber.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Changenumber : MonoBehaviour
+{
+    public int a;
+    public Sprite [] sprites;
+    private Image s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = 0;
+        s = GetComponent <Image>();
+        s.sprite= sprites[0];
+    }
+
+    // Update is called once per frame
+   
+
+    public void Up()
+    {
+        a++;
+        if (a == 10)
+        {
+            a = 0;
+        }
+        Change();
+    }
+    public void Down()
+    {
+        a--;
+        if (a == -1)
+        {
+            a = 9;
+        }
+        Change();
+    }
+    void Change()
+    {
+        s.sprite = sprites[a];
+    }
+}
---
+++ Assets/02.Scripts/UI/Quit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    public void Quits()
+    {
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL("http://google.com");   
+        #else
+            Application.Quit();
+        #endif
+            
+    }
+
+}
