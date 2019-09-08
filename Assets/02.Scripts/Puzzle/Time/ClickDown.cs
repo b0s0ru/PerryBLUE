fed7ecad2a8efd1606d8e2b39d3db0aa991929c2
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-09-09 03:22:10 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/Puzzle/Time/ClickDown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDown : MonoBehaviour
+{
+
+    Changenumber s;
+    private void Start()
+    {
+        s = gameObject.transform.parent.GetComponent<Changenumber>();
+    }
+
+
+
+    void OnMouseUp()
+    {
+        s.Down();
+    }
+
+
+
+
+}
