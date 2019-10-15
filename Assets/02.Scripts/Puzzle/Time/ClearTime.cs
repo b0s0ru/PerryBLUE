a7e18629bbde35bf2f6f822c7abac6b78a35799c
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2019-10-16 03:14:54 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Scripts/Puzzle/Time/ClearTime.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ClearTime : MonoBehaviour
+{
+
+    public ChangeTime[] d;
+    bool win = false;
+    public bool gameclaer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        win = true;
+        for (int i = 0; i < d.Length; i++)
+        {
+
+            if (0 != d[i].a)
+            {
+                win = false;
+            }
+        }
+
+        if (win == true)
+        {
+            gameclaer = true;
+
+            Debug.Log("Gameclear");
+        }
+    }
+
+
+}
